d02924692fa4adc45d11403d986f5b0f5bc333c8
Rui Costa <ruicosta042@gmail.com>
2022-07-10 23:39:29 +0100

    build: add files, types to package

--- sh/build.sh
+++ sh/build.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 main() {
   rm -rf dist
+  tsc --build tsconfig.build.json
+  cp src/calhook.d.ts dist/calhook.d.ts
+  rm dist/date-fns.* dist/helpers.* dist/props.* dist/index.js dist/*.tsbuildinfo
 
   esbuild src/index.ts \
     --bundle \
