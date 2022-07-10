9b0b53c6085146356543445b2e9270710d5c40d1
Rui Costa <ruicosta042@gmail.com>
2022-07-10 21:55:03 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# @tuplo/calhook
---
+++ sh/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+  rm -rf dist
+
+  esbuild src/index.ts \
+    --bundle \
+    --format=esm \
+    --outfile=dist/index.mjs
+}
+
+main
