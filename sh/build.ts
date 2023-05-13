3302476a2bf7c246216c879ed110a04badc115ae
Rui Costa <ruicosta042@gmail.com>
2023-05-13 14:13:23 +0100

    fix: export in cjs format

--- sh/build.ts
+++ sh/build.ts
@@ -6,7 +6,7 @@ async function main() {
 
 	const flags = ["--bundle", "--external:react"];
 
-	await $`esbuild src/index.ts --outfile=dist/index.cjs ${flags}`;
+	await $`esbuild src/index.ts --format=cjs --outfile=dist/index.cjs ${flags}`;
 	await $`esbuild src/index.ts --format=esm --outfile=dist/index.mjs ${flags}`;
 
 	await $`cp src/use-calendar.d.ts dist/use-calendar.d.ts`;
