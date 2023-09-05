44033f1a8604d42aef3f551ec1f44cb4e40bda2f
Rui Costa <ruicosta042@gmail.com>
2023-09-05 19:12:10 +0100

    chore: update deps, security fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
 import path from "node:path";
+
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
